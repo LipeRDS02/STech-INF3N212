b5e68ec70c3028f067095f9837159d2101ed9823
Filipe Reis Dorneles da Silva <182120036@senacinformatica.br>
2023-03-29 19:45:42 -0300

    CadCarro

--- src/model/Carro.java
+++ src/model/Carro.java
@@ -110,9 +110,9 @@ public class Carro {
 
     @Override
     public String toString() {
-        return "\n---" + "\nplaca:\t" + placa + "\nmarca:\t" + marca + "\nmodelo:\t" + modelo + "\nmodelo:\t"
-                + "\nanoFab:\t" + anoFab + "\nanoMod:\t" + anoMod + "\ncor\t" + cor + "\ntpCambio=" + tpCambio +
-                "\ncombustivel:\t" + combustivel +  "\nproprietario\t" + proprietario.getNome() + '}';
+        return "\n---" + "\nplaca:\t" + placa + "\nmarca:\t" + marca + "\nmodelo:\t" + modelo + "\nanoFab:\t" + 
+                anoFab + "\nanoMod:\t" + anoMod + "\ncor\t" + cor + "\ntpCambio=" + tpCambio + "\ncombustivel:\t" + 
+                combustivel +  "\nproprietario\t" + proprietario.getNome();
     }
     
 }
--- src/model/Pessoa.java
+++ src/model/Pessoa.java
@@ -69,6 +69,7 @@ public class Pessoa {
     @Override
     public String toString() {
         
-        return "\n---" +"\nidPessoa:\t" + idPessoa + "\nnome:\t" + nome + "\ncpf:\t" + cpf + "\nendereco:\t" + endereco + "\ntelefone:\t" + telefone ;
+        return "\n---" +"\nidPessoa:\t" + idPessoa + "\nnome:\t" + nome + "\ncpf:\t" + cpf + 
+                "\nendereco:\t" + endereco + "\ntelefone:\t" + telefone;
     }
 }
