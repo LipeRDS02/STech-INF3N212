063a3d3af49e57b96dbdc5f1ad335f37c99c6df1
Filipe Reis Dorneles da Silva <182120036@senacinformatica.br>
2023-03-29 21:47:21 -0300

    atualizaçoes

--- src/inf3n212/INF3N212.java
+++ src/inf3n212/INF3N212.java
@@ -6,6 +6,8 @@ package inf3n212;
 
 import controller.CCarro;
 import controller.CPessoa;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 /**
  *
@@ -15,7 +17,8 @@ public class INF3N212 {
     
     public static CPessoa cadPessoa = new CPessoa();
     public static CCarro cadCarro = new CCarro();
-
+    static Scanner leia = new Scanner(System.in);
+    
     /**
      * @param args the command line arguments
      */
@@ -28,6 +31,43 @@ public class INF3N212 {
         System.out.println("---");
         System.out.println(cadCarro.getCarros());
         
+    }// fime método main
+    
+    public static int leiaNumInt(){
+        Scanner leiaNum = new Scanner(System.in);
+        try{
+           return leiaNum.nextInt();
+        }catch(InputMismatchException i){
+           System.out.println("Erro: " + i.getMessage() + "\nTente Novamente!");
+           leiaNumInt();
+        }
+        return 99;
     }
     
-}
+    public static void menu(){
+        System.out.println("--Menu Principal--");
+        System.out.println("1 - Ger. Pessoa");
+        System.out.println("2 - Ger. Carro");
+        System.out.println("0 - Sair");
+        System.out.print("Digite Aqui: ");
+    }//fim menuP
+    
+    public static void subMenu(int opM){
+        String subM = null;
+        if(opM == 1){
+            subM = "Pessoa";
+        }
+        if(opM == 2){
+            subM = "Carro";
+        }
+        
+        System.out.println("--Ger. " + subM + "--");
+        System.out.println("1 - Cadastrar " + subM);
+        System.out.println("2 - Editar " + subM);
+        System.out.println("3 - Listar " + subM + "s");
+        System.out.println("4 - Deletar " + subM);
+        System.out.println("0 - Voltar");
+        System.out.print("Digite aqui: ");
+    }
+    
+}//fim classe
